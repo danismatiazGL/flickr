6dbd94e8ce2c6ebf4e1d0c73aaaeee323b95425f
Danis Matiaz <dmatiaz@icloud.com>
2018-09-05 09:35:50 -0600

    -Added a data manager to handle the photo's data -Added a networking manager to hanlde the network requests -Added the photos model

--- code/Flickr/Flickr/Managers/DataManager/DataManager.swift
+++ code/Flickr/Flickr/Managers/DataManager/DataManager.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+class DataManager: NSObject {}
+
+enum DataStatus: Int {
+    case success
+    case failure
+}
+
+// MARK: - Data Manager
+extension DataManager {
+    static func savePhotos(photos: String, completion:
+        @escaping (_ isSuccess: Bool, _ response: ServiceStatus, _ errorMessage: String) -> Void) {
+        // To DO: save the response objects locally using Core Data or Realm
+        // to enable the offline functionallity and reuse the data
+    }
+}
+
+extension DataManager {
+    static func serializePhotos(data: Data) -> [Photo] {
+        var newPhotos: [Photo] = []
+        do {
+            let json = try JSON(data: data)
+            let photos = json[FlickrAPIKey.photos.rawValue][FlickrAPIKey.photo.rawValue].arrayValue
+            for photo in photos {
+                let newPhoto = self.photo(with: photo)
+                newPhotos.append(newPhoto)
+            }
+        } catch {
+            print("To Do: Error handling here")
+        }
+        return newPhotos
+    }
+
+    static func photo(with data: JSON) -> Photo {
+        let photoId: String = data[FlickrAPIKey.photoId.rawValue].stringValue
+        let serverId: String = data[FlickrAPIKey.serverId.rawValue].stringValue
+        let farmId: String = data[FlickrAPIKey.farmId.rawValue].stringValue
+        let secretId: String = data[FlickrAPIKey.secretId.rawValue].stringValue
+        let url: String = data[FlickrAPIKey.url.rawValue].stringValue
+        let newPhoto: Photo = Photo.init(photoId: photoId,
+                                         serverId: serverId,
+                                         farmId: farmId,
+                                         secretId: secretId,
+                                         url: url)
+        return newPhoto
+    }
+}
--- code/Flickr/Flickr/Managers/FlickrManager/NetworkManager.swift
+++ code/Flickr/Flickr/Managers/FlickrManager/NetworkManager.swift
@@ -7,3 +7,72 @@
 //
 
 import Foundation
+
+import Foundation
+import Alamofire
+
+class NetworkManager: NSObject {}
+
+enum ServiceStatus: Int {
+    case failure
+    case success
+}
+
+// MARK: - Request URL Builder
+extension NetworkManager {
+    private static func baseRequest(service: FlickrService) -> String {
+        if service == .search {
+            return "\(NetworkConstant.flickrBaseURL)\(service.rawValue)"
+        } else {// we don't have other services yet
+            return ""
+        }
+    }
+
+    private static func searchRequest(query: String, page: Int) -> String {
+        let baseURL = self.baseRequest(service: .search)
+        var queryEncoded = ""
+        if let encodedString = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+            queryEncoded = encodedString
+        }
+        let baseParameters = "\(baseURL)\(self.flickerAPIKey())\(self.searchParameters())"
+        let searchParameters = "\(self.searchQuery(query: queryEncoded))\(self.searchPage(page: page))"
+        return "\(baseParameters)\(searchParameters)"
+    }
+
+    private static func flickerAPIKey() -> String {
+        return "&\(FlickrServiceParameter.apiKey)\(NetworkConstant.flickrAPIKey)"
+    }
+
+    private static func searchParameters() -> String {
+        return "&\(FlickrServiceParameter.apiKey)\(NetworkConstant.flickrAPIKey)"
+    }
+
+    private static func searchQuery(query: String) -> String {
+        return "&\(FlickrServiceParameter.text)\(query)"
+    }
+
+    private static func searchPage(page: Int) -> String {
+        return "&\(FlickrServiceParameter.page)\(page)"
+    }
+}
+
+// MARK: - Request URL Builder
+extension NetworkManager {
+    static func searchPhotos(query: String, page: Int, completion:
+        @escaping (_ isSuccess: ServiceStatus, _ response: [Photo]?, _ errorMessage: String) -> Void) {
+        let requestURL = NetworkManager.searchRequest(query: query, page: page)
+        Alamofire.request(requestURL).responseData { (response) in
+            switch response.result {
+            case .success:
+                if let responseData = response.result.value {
+                    let newPhotos = DataManager.serializePhotos(data: responseData)
+                    completion(.success, newPhotos, "")
+                } else {
+                    completion(.failure, nil, "No data available")
+                }
+            case .failure(let error):
+                completion(.failure, nil, error.localizedDescription)
+            }
+        }
+    }
+}
